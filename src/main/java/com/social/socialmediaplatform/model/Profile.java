a35c0fb9c519fa03e897075d2beb866aa393695b
Anagha Nirgude <anagha.nirgude1@gmail.com>
2026-02-03 17:16:38 +0530

    one to one relation

---
+++ src/main/java/com/social/socialmediaplatform/model/Profile.java
@@ -0,0 +1,15 @@
+package com.social.socialmediaplatform.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="social_user")
+    private SocialUser socialUser;
+}
